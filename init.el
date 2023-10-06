39285eefab072fface394a9b109fc2974898bb59
Aaron Cohen <aaroncohendev@gmail.com>
2023-10-06 17:18:06 +0300

    add lsp mode

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 ; initialize package management
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 ;; Ensure 'use-package' is installed
@@ -19,6 +20,9 @@
   :hook (after-init . global-company-mode))
 (use-package go-mode
   :ensure t)
+(use-package lsp-mode
+  :hook (go-mode . lsp)
+  :commands lsp)
 (use-package magit
   :ensure t)
 (use-package yasnippet
@@ -60,7 +64,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme yasnippet-snippets yasnippet magit use-package)))
+   '(lsp-mode zenburn-theme yasnippet-snippets yasnippet magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
